e9bce166d534bd2ce3d1cf47daf745a224012b02
steven <steven.mandau@gmx.de>
2024-03-22 12:52:48 +0100

    init react frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+
+  return (
+    <>
+        <h1>Supermarket Sim</h1>
+        <p>
+            Die BrowserApp ist bereit für Entwicklung. <br />
+        </p>
+    </>
+  )
+}
