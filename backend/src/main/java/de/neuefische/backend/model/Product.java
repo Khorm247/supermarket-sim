0cd35b2113c698685496e89c2cc37abaa50658e0
steven <steven.mandau@gmx.de>
2024-03-27 11:59:55 +0100

    add ProductDto model updated Product model

--- backend/src/main/java/de/neuefische/backend/model/Product.java
+++ backend/src/main/java/de/neuefische/backend/model/Product.java
@@ -1,19 +1,24 @@
 package de.neuefische.backend.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @RequiredArgsConstructor
+@Data
+@Builder
 public class Product {
+    @Id
+    private String productId;
     private String name;
     private String producer;
     private BigDecimal pricePerBox;
     private BigDecimal fairMarketValue;
     private BigDecimal yourPrice;
-    private String boxSize;
-    private int boxSizeInUnits;
     private int itemsPerBox;
 }
