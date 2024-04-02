cbfbde65cb1aeb67ee4cd542155379ec4c913dda
steven <steven.mandau@gmx.de>
2024-04-02 15:37:13 +0200

    add routes to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from "./components/NavBar.tsx";
+import {Route, Routes} from "react-router-dom";
+import ProductList from "./pages/products/ProductList.tsx";
 
 export default function App() {
 
   return (
     <>
+        <Routes>
+            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/api/markets" element={<h1>Home</h1>}/>
+            <Route path="/api/products" element={<ProductList/>}/>
+            <Route path="/api/markets/:id" element={<h1>Home</h1>}/>
+            <Route path="/api/products/:id" element={<h1>Home</h1>}/>
+        </Routes>
         <NavBar />
     </>
   )
