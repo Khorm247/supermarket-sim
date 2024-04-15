002bed6801ae57a5e5d863a39175ad63c5f61b03
steven <steven.mandau@gmx.de>
2024-04-15 23:44:39 +0200

    update Upgrade.tsx styling and layout

--- frontend/src/pages/market/Upgrade.tsx
+++ frontend/src/pages/market/Upgrade.tsx
@@ -1,7 +1,8 @@
-import {Button} from "react-bootstrap";
+import {Button, Col, Row, Card} from "react-bootstrap";
 import {Inventory} from "../../types/Inventory.ts";
 import {useNavigate} from "react-router-dom";
 import Container from "react-bootstrap/Container";
+
 
 type UpgradeProps = {
     addCategory: (category: string) => void,
@@ -18,13 +19,72 @@ export default function Upgrade(props: Readonly<UpgradeProps>) {
     }
 
     return (
-        <Container>
-            <h1>Laden ausbauen</h1>
-            <p><Button onClick={() => handleUpgradeClick("FRUITS")}>Fruchtlizenz kaufen</Button></p>
-            <p><Button onClick={() => handleUpgradeClick("CLEANING_SUPPLIES")}>Reinigungsproduktlizenz kaufen</Button></p>
-            <p><Button>Regalplatz erweitern</Button></p>
-            <p><Button>Lagerplatz erweitern</Button></p>
-            <p><Button>Personal schulen</Button></p>
+        <Container className={"py-3"}>
+            <Row>
+                <div className="d-flex justify-content-center">
+                    <h1>Laden ausbauen</h1>
+                </div>
+            </Row>
+            <Row>
+            <Col>
+                    <Card style={{height: '22rem'}}>
+                        <Card.Img style={{height: '12rem'}} variant="top" src="https://source.unsplash.com/random/120×100/?shelf" />
+                        <Card.Body>
+                            <Card.Title>Regalplatz erweitern</Card.Title>
+                            <Card.Text>
+                                erweitere deinen Regalplatz um 10 Plätze
+                            </Card.Text>
+                            <Button className="button-secondary">100€</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col>
+                    <Card style={{height: '22rem'}}>
+                        <Card.Img style={{height: '12rem'}} variant="top" src="https://source.unsplash.com/random/120×100/?storage" />
+                        <Card.Body>
+                            <Card.Title>Lagerplatz erweitern</Card.Title>
+                            <Card.Text>
+                                erweitere dein Lager um 20 Plätze
+                            </Card.Text>
+                            <Button className="button-secondary">150€</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <div className="d-flex justify-content-center">
+                    <h1>Lizenzen kaufen</h1>
+                </div>
+            </Row>
+
+            <Row>
+                <Col>
+                    <Card style={{height: '22rem'}}>
+                        <Card.Img style={{height: '12rem'}} variant="top" src="https://source.unsplash.com/random/120×100/?fruit" />
+                        <Card.Body>
+                            <Card.Title>Fruchtlizenz</Card.Title>
+                            <Card.Text>
+                                erweitere dein Sortiment um Früchte
+                            </Card.Text>
+                            <Button onClick={() => handleUpgradeClick("FRUITS")} className="button-secondary">200€</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+
+                <Col>
+                    <Card style={{height: '22rem'}}>
+                        <Card.Img style={{height: '12rem'}} variant="top" src="https://source.unsplash.com/random/120×100/?cleaning" />
+                        <Card.Body>
+                            <Card.Title>Reinigungsproduktlizenz</Card.Title>
+                            <Card.Text>
+                                erweitere dein Sortiment um Reinigungsprodukte
+                            </Card.Text>
+                            <Button onClick={() => handleUpgradeClick("CLEANING_SUPPLIES")} className="button-secondary">250€</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
         </Container>
     )
 }
